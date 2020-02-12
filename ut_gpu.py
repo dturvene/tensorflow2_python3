5b3a037cd9dc4285eaf293383dd00660ea999d24
David Turvene <dturvene@dahetral.com>
2020-02-12 11:42:35 -0500

    home push 200212

--- ut_gpu.py
+++ ut_gpu.py
@@ -3,6 +3,7 @@
 unit test for packages and keras support
 
 * https://www.tensorflow.org/tutorials/keras/regression
+* https://www.tensorflow.org/guide/gpu
 """
 
 import sys
@@ -15,19 +16,32 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-def cpu():
+def ut_devices():
+    '''probe devices'''
+    gpus = tf.config.list_physical_devices('GPU')
+    cpus = tf.config.list_physical_devices('CPU')
+    print(f'GPUs: {gpus} CPUs: {cpus}')
+
+def conv2d_cpu():
     with tf.device('/cpu:0'):
         random_image_cpu = tf.random.normal((100, 100, 100, 3))
         net_cpu = tf.keras.layers.Conv2D(32, 7)(random_image_cpu)
         return tf.math.reduce_sum(net_cpu)
 
-def gpu():
+def conv2d_gpu():
     with tf.device('/device:GPU:0'):
         random_image_gpu = tf.random.normal((100, 100, 100, 3))
         net_gpu = tf.keras.layers.Conv2D(32, 7)(random_image_gpu)
         return tf.math.reduce_sum(net_gpu)
-        
-def ut_cpu_gpu():
+
+def ut_cpu():
+    '''simple perftest of cpu'''
+    print('convolve 32x7x7x3 filter over random 100x100x100x3 images '
+          '(batch x height x width x channel). Sum of ten runs.')
+    cpu_time = timeit.timeit('conv2d_cpu()', number=10, setup="from __main__ import conv2d_cpu")
+    print(f'CPU (s): {cpu_time}')
+   
+def ut_gpu():
     '''
     shamelessly culled from
     https://colab.research.google.com/notebooks/gpu.ipynb
@@ -39,38 +53,32 @@ def ut_cpu_gpu():
               'reload this unit test')
         return(0)
 
-    # We run each op once to warm up; see: https://stackoverflow.com/a/45067900
+    # We run once to warm up; see: https://stackoverflow.com/a/45067900
     # this is only necessary for the first time a GPU is accessed
-    cpu()
-    print('warming up gpu')
+    print('warming up gpu...')
     gpu()
 
-    # Run CPU N times then GPU and compare runtime ratio
-    print('Time (s) to convolve 32x7x7x3 filter over random 100x100x100x3 images '
-          '(batch x height x width x channel). Sum of ten runs.')
-    print('CPU (s):')
-    cpu_time = timeit.timeit('cpu()', number=10, setup="from __main__ import cpu")
-    print(cpu_time)
-    print('GPU (s):')
-    gpu_time = timeit.timeit('gpu()', number=10, setup="from __main__ import gpu")
-    print(gpu_time)
-    print('GPU speedup over CPU: {}x'.format(int(cpu_time/gpu_time)))    
+    # Run GPU N times, similar to cpu and compare runtime ratio
+    gpu_time = timeit.timeit('conv2d_gpu()', number=10, setup="from __main__ import conv2d_gpu")
+    print(f'GPU (s): {gpu_time}')
 
 class Ut(unittest.TestCase):
     def setUp(self):
-        pass
+        ut_devices()
     def tearDown(self):
         pass
     #@unittest.skip('good')
     def test1(self):
-        tf.test.is_gpu_available()
+        res=tf.reduce_sum(tf.random.normal([1000, 1000]))
+        print(f'simple reduce_sum:{res}')
     #@unittest.skip('good')
     def test2(self):
-        print(tf.reduce_sum(tf.random.normal([1000, 1000])))
+        ut_cpu()
+    #@unittest.skip('good')        
     def test3(self):
-        ut_cpu_gpu()        
+        ut_gpu()
 
 if __name__ == '__main__':
     # exec(open('./ut_gpu.py').read())
     print('tf={}'.format(tf.__version__))
-    unittest.main(exit=False)
+    # unittest.main(exit=False)
